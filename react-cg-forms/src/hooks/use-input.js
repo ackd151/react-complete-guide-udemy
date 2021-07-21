55ef60c1440241992868ca92dc4a13776a3721a2
Daniel Ackerman <drackerm@uci.edu>
2021-07-21 18:32:20 -0500

    Changed custom useInput hook to useReducer for state mgmt. Finished section 16, forms/input validations.

--- react-cg-forms/src/hooks/use-input.js
+++ react-cg-forms/src/hooks/use-input.js
@@ -1,27 +1,52 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === "RESET") {
+    return initialInputState;
+  }
+
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [touched, setTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && touched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({
+      type: "INPUT",
+      value: event.target.value,
+    });
   };
 
   const inputBlurHandler = (event) => {
-    setTouched(true);
+    dispatch({
+      type: "BLUR",
+    });
   };
 
   const reset = () => {
-    setTouched(false);
-    setEnteredValue("");
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
