ee704e7313357829b9242349866b9d1c9f11da0c
Daniel Ackerman <drackerm@uci.edu>
2021-07-27 20:55:09 -0500

    Working on authentication, section 22.

---
+++ react-cg-auth/src/components/Profile/ProfileForm.js
@@ -0,0 +1,51 @@
+import classes from "./ProfileForm.module.css";
+import { useContext, useRef } from "react";
+import AuthContext from "../../store/auth-context";
+
+const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // add validation
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCrtNEzOb1rJ8s5uNPUgNiwQqtjnzhGSQw",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      // assumption: always succeeds
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input
+          ref={newPasswordInputRef}
+          minLength="7"
+          type="password"
+          id="new-password"
+        />
+      </div>
+      <div className={classes.action}>
+        <button>Change Password</button>
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
